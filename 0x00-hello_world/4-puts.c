2b2e8f8cb3805384b0b9c7d104dab7c8f0e09036
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-09-08 14:57:29 -0700

    Task4-8

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+/**
+ *
+ * * main - Printing in C programming
+ *
+ * *
+ *
+ * * Return: Always 0 (It was Successful)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual
+
+			puzzle");
+
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - Printing a text
+ *
+ * *
+ *
+ * * Return: Always 0 (It was a Success)
+ *
+ * */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of
+
+			art,\n");
+
+	return (0);
+
+}
