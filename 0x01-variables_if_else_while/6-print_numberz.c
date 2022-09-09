ad9be9ab8b8ec94e1f914dfc9d496fbcad35dc89
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-09-09 16:06:12 -0700

    Task 6 commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,25 +4,17 @@
  *
  * main - Prints the numbers since 0 to 9
  *
+ * Description: using main function
  * Return: Always (Success)*
  */
 
 int main(void)
-
 {
-
 	int n;
-
 	for (n = '0'; n <= '9'; n++)
-
 	{
-
 		putchar(n);
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
