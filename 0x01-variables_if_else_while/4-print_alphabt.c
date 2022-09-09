641795d3c91d0d541c1994db21a6fa9276a43413
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-09-09 15:54:24 -0700

    Task 4 commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,24 +7,23 @@
  */
 
 int main(void)
-
 {
 
 	char c;
 	
 	for (c = 'a'; c <= 'z'; c++)
-
 	{
-	
+
 		if (c != 'e' && c != 'q')
-
+		
 		{
-			putchar(c);
 
+			putchar(c);
+		
 		}
 
 	}
-	
+
 	putchar('\n');
 
 	return (0);
