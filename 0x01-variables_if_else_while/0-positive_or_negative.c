6c0e89b74171cf63416335acf6b8e153772ea765
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-09-09 14:02:21 -0700

    Task 0 commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,7 +8,7 @@
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
- * * Return: 0
+ * * Return: Always 0 (Successful)
  */
 
 int main(void)
@@ -19,7 +19,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+		/* your code goes there */
 	if (n > 0)
 
 	{
