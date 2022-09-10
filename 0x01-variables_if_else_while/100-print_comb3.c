f4ad296b2b7b0592013a994b3eb6b73ff252f53e
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-09-10 01:51:03 -0700

    Task 10 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+
 /**
- * main - print single digit numbers starting from 0
- *
- * Return: returns zero at the end
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int i, j;
-	for (i = 0; i <= 9; i++)
+       	int n, m;
+	
+	for (n = 48; n <= 56; n++)
 	{
-		for (j = 1; j <= 9; j++)
+		for (m = 49; m <= 57; m++)
 		{
-			if (j > i)
+			if (m > n)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-
-				if (i != 8)
+				putchar(n);
+				putchar(m);
+				
+				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
