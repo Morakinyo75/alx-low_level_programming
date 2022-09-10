a3ad242102f9e4979cba9bcff63a85ffd073498a
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-09-09 20:41:54 -0700

    Task 9 commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,22 +5,18 @@
  *
  * Return: Always (Success);
  */
-
 int main(void)
 {
     int c;
-
     for (c = '0'; c <= '9'; c++)
     {
-	    putchar(c);
-	    
+	    putchar(c);  
 	    if (c != '9')
 	    {
     		    putchar(',');
 		    putchar(' ');
-	    }	
+	    }
     }
 	 putchar('\n');
-
 	 return (0);
 }
