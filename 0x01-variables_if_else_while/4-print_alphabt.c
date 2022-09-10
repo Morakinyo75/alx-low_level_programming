2597bd740b7b8b5ef45d8e962ec38c17aebe8c0e
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-09-09 22:03:17 -0700

    Task 4 commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
-
 /**
- * main - Ommit some alphabet
- *
- * Return: Always (Success)
+ * main - print if the number is postive, zero, or negative
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
  */
-
 int main(void)
 {
-
-	char c;
+	char ch;
 	
-	for (c = 'a'; c <= 'z'; c++)
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-
-		if (c != 'e' && c != 'q')
-		
+		if (ch != 'e' && ch != 'q')
 		{
-
-			putchar(c);
-		
+			putchar(ch);
 		}
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
