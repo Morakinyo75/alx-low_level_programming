253c8d7e19d2bb1d81972b0caa9fc673b201001f
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-09-09 21:56:29 -0700

    Task 6 commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-
 /**
- *
  * main - Prints the numbers since 0 to 9
- *
+ * Descritpion: using the main function
+ * this programme prints "Programmig is positive, zero or negative"
  * Return: Always (Success)*
  */
-
 int main(void)
 {
-
 	int n;
 
 	for (n = '0'; n <= '9'; n++)
-
 	{
-
 		putchar(n);
-
 	}
-
 	putchar('\n');
-
 	return (0);
 }
