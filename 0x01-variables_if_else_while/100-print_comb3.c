8c422e68852ade7fee08e9690ca331c8a7b13374
Morakinyo75 <akinwandeemmanuel899@gmail.com>
2022-09-09 22:57:26 -0700

    Task 10 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
-* main - print single digit numbers starting from 0
+ * main - print single digit numbers starting from 0
  *
  * Return: returns zero at the end
  */
 int main(void)
 {
 	int i, j;
-	
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 1; j <= 9; j++)
@@ -16,17 +15,15 @@ int main(void)
 			{
 				putchar(i + '0')
 				putchar(j + '0');
-				
+
 				if (i != 8)
 				{
 					putchar(',');
-					putchar(' ')
+					putchar(' ');
 				}
-
 			}
 		}
 	}
 	putchar('\n')
 	return (0);
-
 }
