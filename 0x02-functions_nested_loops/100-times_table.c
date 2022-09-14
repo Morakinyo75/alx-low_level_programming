c69dbb21943c0b886ed5d74fc9c8163ef4bc0e4c
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-14 11:01:09 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,19 +13,19 @@ void print_times_table(int n)
 		for (num = 0; num <= n; num++)
 		{
 			_putchar('0');
-
 			for (mult = 1; mult <= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
 				prod = num * mult;
-
 				if (prod <= 99)
+				{
 					_putchar(' ');
-
+				}
 				if (prod <= 9)
+				{
 					_putchar(' ');
-
+				}
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
@@ -35,10 +35,8 @@ void print_times_table(int n)
 				{
 					_putchar((prod / 10) + '0');
 				}
-				
 				_putchar((prod % 10) + '0');
 			}
-			
 			_putchar('\n');
 		}
 	}
