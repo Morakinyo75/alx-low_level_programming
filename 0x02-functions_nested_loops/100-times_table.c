13135290600c360ae37e46e357d4a52119be5461
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-14 12:33:48 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,9 +14,11 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b<= n; b++)
+			for (b = 0; b <= n; b++)
 			{
-				c = a * b; d = c / 100; e = (c / 10) % 10;
+				c = a * b;
+				d = c / 100;
+				e = (c / 10) % 10;
 				f = (c % 100) % 10;
 				if (b == 0)
 				{
