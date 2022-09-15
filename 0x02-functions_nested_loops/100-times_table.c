b4527bf648362c2838055c38400b06438634ebbe
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-15 07:59:55 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,22 +1,41 @@
-#include <stdio.h>
-#include <main.h>
-
-/** primt_number - Prints a number
- * @n: the number to print
+#include "main.h"
+/**
+ * print_times_table - prints the  times table with
+ * parameter
+ * @n: parameter
+ * Return: returns nothing
  */
-
-void print_number(int n)
+void print_times_table(int n)
 {
-	unsigned int num = n;
+	int digit, mult, result;
 
-	if (n < 0)
+	if (n <= 15 && n >= 0)
 	{
-		putchar ('-');
-		num = -num;
+		for (digit = 0; digit <= n; digit++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				result = digit * mult;
+				if (result <= 99)
+					_putchar(' ');
+				if (result <= 9)
+					_putchar(' ');
+				if (result >= 100)
+				{
+					_putchar((result / 100) + '0');
+					_putchar((result / 10) % 10 + '0');
+				}
+				else if (result <= 99 && result >= 10)
+				{
+					_putchar((result / 10) + '0');
+				}
+				_putchar((result % 10) + '0');
+			}
+			_putchar('\n');
+		}
 	}
-	if (n > 9)
-	{
-		print_number(num / 10);
-	}
-	putchar(num % 10 + '0')
 }
