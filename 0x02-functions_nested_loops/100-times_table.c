bf8105e0883dc50fb6aba3367e0f5faa65066499
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-15 05:11:51 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,22 @@
-#include "main.h" 
-/** 
- * print_times_table - print multiplication table up to n 
- * @n: integer argument 
+#include <stdio.h>
+#include <main.h>
+
+/** primt_number - Prints a number
+ * @n: the number to print
  */
-void print_times_table(int n)
+
+void print_number(int n)
 {
-	int row;
-	int column;
-	int product;
-	
-	if (n >= 0 && n <= 15)
+	unsigned int num = n;
+
+	if (n < 0)
 	{
-		for (row = 0; row <= n; row++)
-		{
-			for (column = 0; column <= n; column++)
-			{
-				product = (row * column);
-				if (column == 0)
-					_putchar('0' + product);
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					if (product <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + product);
-					}
-					else if (product > 9 && product < 100)
-					{
-						_putchar(' ');
-						_putchar('0' + (product / 10));
-						_putchar('0' + (product % 10));
-					}
-					else if (product >= 100)
-					{
-						_putchar('0' + (product / 100));
-						_putchar('0' + ((product / 10) % 10));
-						_putchar('0' + (product % 10));
-					}
-				}
-			}
-			_putchar('\n');
-		}
+		putchar ('-');
+		num = -num;
 	}
+	if (n > 9)
+	{
+		print_number(num / 10);
+	}
+	putchar(num % 10 + '0')
 }
