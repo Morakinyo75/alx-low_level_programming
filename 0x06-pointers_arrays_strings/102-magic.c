0add547deb5cde1fe0884ee68409d33662625e86
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-22 08:10:10 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: add a line in a code that uses printf
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+/*
+ * Description: a program prints a[2] = 98, followed by a new line
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
+	p(5) = 98;
+ /* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
+}
