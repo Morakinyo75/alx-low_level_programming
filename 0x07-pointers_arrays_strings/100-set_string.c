9ab3250853004774a9f26f97fa8a4250e010f81b
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-26 14:42:46 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * see_string - sets the value of pointer to char
+ * @s: pointer to change
+ * @to: string to change pointer to
+ *
+ * Return: void
+ */
+void set_string(char **s, char to)
+{
+	*s = to;
+}
