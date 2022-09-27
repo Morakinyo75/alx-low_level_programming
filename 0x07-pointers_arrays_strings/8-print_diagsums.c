2075bbc09d846b89d8cc13aeda8691f1828f62cd
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-26 20:46:46 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,6 +15,6 @@ void print_diagums(int *a, int size)
 		sum1 += *(a + (size * i + i));
 		sum2 += *(a + (size * i + size - 1 - i));
 	}
-	printf("%d, ", sum1);
+	printf("%d", sum1);
 	printf("%d\n", sum2);
 }
