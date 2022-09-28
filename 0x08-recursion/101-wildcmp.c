0e0eee5edab675b7b33c6e2437d386d2cc0da69c
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-28 11:40:56 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,6 +13,6 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == *s1)
 		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s != '\0' && wildcmp(s1 + 1, s2)));
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	return (0);
 }
