27a67416bad5850a29332ed8062fff982c643399
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-28 06:42:05 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h" 
+
 /**
  * puts_recursion - prints a string followed by a new line
  * @s - string to be printed
  *
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
@@ -16,3 +18,4 @@ void _puts_recursion(char *s)
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
+}
