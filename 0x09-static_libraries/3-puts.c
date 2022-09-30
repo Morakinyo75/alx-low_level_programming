2de7ea2a852f2f54718d5788789dd48320932e57
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-30 15:23:12 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: string to be printed
+ *
+ * Description: print a string
+ * On success: return no error
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
