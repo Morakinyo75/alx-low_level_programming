132692c018dbbe421e750fe6d35ddd0955c01ac2
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-09-30 17:08:38 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,26 +21,27 @@ int main(int argc, char *argv[])
 	{
 		coins++;
 		if ((cents - 25) >= 0)
+		{
 			cents -= 25;
-		continue;
-	}
-	if ((cents - 10) >= 0)
-	{
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
 		cents -= 10;
 		continue;
-	}
-	if ((cents - 5) >= 0)
-	{
+		}
+		if ((cents - 5) >= 0)
+		{
 		cents -= 5;
 		continue;
-	}
-	if ((cents - 2) >= 0)
-	{
+		}
+		if ((cents - 2) >= 0)
+		{
 		cents -= 2;
 		continue;
+		}
+		cents--;
 	}
-	cents--;
-}
-printf("%d\n", coins);
-return (0);
+	printf("%d\n", coins);
+	return (0);
 }
