c84843c8e1b221d06caaf36a2d83d979a6343e90
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-10-03 08:06:12 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size ==0)
 		return (NULL);
-	array = malloc(size of (char) * size);
+	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
