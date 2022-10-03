bf2a22b7bca15812aa5f934d7f32d61786228c12
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-10-03 08:35:03 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,13 +15,13 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (index = 0, str[index]; index++)
+	for (index = 0; str[index]; index++)
 		len++;
 	cpy = malloc(sizeof(char) * (len + 1));
 
 	if (cpy == NULL)
 		return (NULL);
-	for (index = 0, str[index]; index++)
+	for (index = 0; str[index]; index++)
 	{
 		cpy[index] = str[index];
 	}
