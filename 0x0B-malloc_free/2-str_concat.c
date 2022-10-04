3fef5709c49a2dd3aa1b3a5177f3e971838973e7
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-10-04 02:49:04 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,16 +26,16 @@ char *str_concat(char *s1, char *s2)
 	if (concat == NULL)
 		return (NULL);
 	i = ci = 0;
-	while (s1[1] != '\0')
+	while (s1[i] != '\0')
 	{
-		concat[1] = s1[i];
+		concat[i] = s1[i];
 		i++;
 	}
-	while (s2[1] != '\0')
+	while (s2[ci] != '\0')
 	{
-		concat[1] = s2[ci];
+		concat[i] = s2[ci];
 		i++, ci++;
 	}
-	concat[1] = '\0';
+	concat[i] = '\0';
 	return (concat);
 }
