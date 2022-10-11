55cbe7bac2b12ec7d09c4b519f849e7f124da76d
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-10-10 19:34:54 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * init_dog - initializes a variale of type struct doh
+ * @d: pointer to structure
+ * @name: fist member
+ * @age: second member
+ * @owner: third member
+ *
+ * Reture: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == 0)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
