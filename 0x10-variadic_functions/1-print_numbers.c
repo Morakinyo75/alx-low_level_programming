4872d66bf16a17299bf3d87531ffa0bcb3019d5b
Morakimyo75 <akinwandeemmanuel899@gmail.com>
2022-10-13 10:24:18 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,9 +1,11 @@
-#include <stdarg>
+#include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
 /** print_numbers - prints numbers, followed by a new line
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  *
+ * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
